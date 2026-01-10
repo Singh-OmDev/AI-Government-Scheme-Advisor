a1ca841ea41eb91aa625e5f31203d0058be61488
Om <omsingh8400@gmail.com>
2026-01-10 13:19:33 +0530

    Add API rewrite to vercel.json

--- server/debug_env_check.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-console.log('--- ENV CHECK ---');
-console.log('CLERK_SECRET_KEY present:', !!process.env.CLERK_SECRET_KEY);
-console.log('MONGODB_URI present:', !!process.env.MONGODB_URI);
-console.log('GROQ_API_KEY present:', !!process.env.GROQ_API_KEY);
-console.log('--- END CHECK ---');
